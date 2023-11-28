63537990654661bca3eb9ad0701682987e409272
AlecoPB <ajoaquinpg@gmail.com>
2023-11-28 07:49:36 -0800

    debugging

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,11 +3,12 @@
  *_strcpy - Copies a string to another
  *@dest: string to be copied to
  *@src: source string
- *Return: I actually don't know
+ *Return: returns a pointer to the original start of dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
+char *original_dest = dest;
 while (*src)
 {
 *dest = *src;
@@ -15,4 +16,5 @@ while (*src)
 ++dest;
 }
 *dest = *src;
+return original_dest;
 }
