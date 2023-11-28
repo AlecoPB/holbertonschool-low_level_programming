0fcd9290300e17a9d5940db318822a03283a1134
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-28 09:34:24 +0100

    Update 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,8 @@
 /*
-  * updates the value it points to to 98 
-  *
-  * It returns nothing
-  *
-  */
+* updates the value it points to to 98
+* It returns nothing
+*/
 void reset_to_98(int *n)
 {
-  *n = 98;
+    *n = 98;
 }
