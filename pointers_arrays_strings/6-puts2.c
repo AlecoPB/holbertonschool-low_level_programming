e0c13fad9bc248bcc0ebdcd209c434616daf5b84
AlecoPB <ajoaquinpg@gmail.com>
2023-11-28 04:42:39 -0800

    Added a funtion that prints every other character or a string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *puts2 - Prints every other character on a string
+ *@str: The string to be printed
+ *Return: returns nothing
+ */
+
+void puts2(char *str)
+{
+const char *s = str;
+putchar *s;
+s += 2;
+while (*s)
+{
+putchar(*s);
+s += 2;
+}
+putchar('\n');
+}
