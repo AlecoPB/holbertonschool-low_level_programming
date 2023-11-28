3a2ddf393fb762c72f1f5d24449c7cf3c3ad2023
AlecoPB <ajoaquinpg@gmail.com>
2023-11-28 07:45:11 -0800

    Added string copying function

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *_strcpy - Copies a string to another
+ *@dest: string to be copied to
+ *@src: source string
+ *Return: I actually don't know
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+while (*src)
+{
+*dest = *src;
+++src;
+++end;
+}
+*dest = *src;
+}
