22e046b2a7ef8dbdea287dd3c82940bc8b5bcf16
AlecoPB <ajoaquinpg@gmail.com>
2023-11-29 05:45:47 -0800

    I really scorn the use of doubly linked lists

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *add_dnodeint - Adds a new node at the beginning of a dlist
+ *@head: pointer to the list
+ *@n: Element to be added
+ *Return: Adress of the new elemnt
+ */
+
+size_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new_node;
+
+if (head == NULL)
+return NULL;
+
+new_node = malloc(sizeof(dlistint_t))M
+if (new_node == NULL)
+return NULL;
+
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = *head;
+if (*head != NULL)
+(*head)->prev = new_node;
+
+*head = new_node;
+
+return (new_node);
+}
