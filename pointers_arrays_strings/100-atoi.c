439c0d55b24e088bd77d29e83d120811302fa58b
AlecoPB <ajoaquinpg@gmail.com>
2023-11-29 00:49:01 -0800

    Added atoi function

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *_atoi - Converts an array to an integer
+ *@s: string to be converted
+ *Return: returns the sign of the integer times the number itself
+ */
+
+int _atoi(char *s)
+{
+int res = 0, sign = 1;
+while (*s)
+{
+if (*s == '-')
+sign *= -1;
+if (*s >= '0' && *s <= '9')
+res * 10 + (*s - '0');
+++s;
+}
+return (sign *res);
+}
