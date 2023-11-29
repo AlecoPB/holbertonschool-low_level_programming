f34c148184225c9cdb9e9c0eac6483c30c363cca
AlecoPB <ajoaquinpg@gmail.com>
2023-11-29 01:00:59 -0800

    Added break

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,8 +12,14 @@ while (*s)
 {
 if (*s == '-')
 sign *= -1;
-if (*s >= '0' && *s <= '9')
+else if (*s >= '0' && *s <= '9')
+{
 res = res * 10 + (*s - '0');
+if ((*s + 1) < '0' || *(s + 1) > '9')
+{
+break;
+}
+}
 ++s;
 }
 return (sign *res);
