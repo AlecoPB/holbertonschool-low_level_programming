e3576f241a9c1f7eecd7183b101e314b55c293a5
AlecoPB <ajoaquinpg@gmail.com>
2023-11-29 07:41:41 -0800

    Standarizing names

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,6 +4,7 @@
 
 /**
  *dlistint_t *instert_dnodeint_at_index - inserts a node at index
+ *@h: pointer to the list
  *@idx: where to insert
  *@n: Element to be added
  *Return: Adress of the new elemnt
@@ -14,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 dlistint_t *new_node, *current;
 unsigned int i;
 
-if (head == NULL)
+if (h == NULL)
 return (NULL);
 
 new_node = malloc(sizeof(dlistint_t));
@@ -23,15 +24,15 @@ return (NULL);
 
 new_node->n = n;
 
-if (*head == NULL && idx == 0)
+if (*h == NULL && idx == 0)
 {
 new_node->prev = NULL;
 new_node->next = NULL;
-*head = new_node;
+*h = new_node;
 }
 else
 {
-current = *head;
+current = *h;
 for (i = 0; i < index && current != NULL; i++)
 
 current = current->next;
@@ -46,7 +47,7 @@ new_node->next = current;
 if (current->prev != NULL)
 current->prev->next = new_node;
 else
-*head = new_node;
+*h = new_node;
 current->prev = new_node;
 }
 return (new_node);
