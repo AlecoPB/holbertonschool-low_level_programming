b6789d0f367a3c2aa0da971444da4a50ae261099
AlecoPB <ajoaquinpg@gmail.com>
2023-11-29 14:35:22 -0800

    Remake of 7-inster_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,52 +16,43 @@ dlistint_t *new_node, *current;
 unsigned int i;
 
 if (h == NULL)
-return (NULL);
+  return (NULL);
 
 new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
+ if (new_node == NULL)
+   {
+     return (NULL);
+   }
 
-new_node->n = n;
+ new_node->n = n;
+ if (idx == 0)
+   {
+     new_node->next = *h;
+     new_node->prev = NULL;
+     if (*h != NULL)
+       (*h)->prev = new_node;
+     h* = new_node;
+     return (new_node);
+   }
 
-if (*h == NULL && idx == 0)
-{
-new_node->prev = NULL;
-new_node->next = NULL;
-*h = new_node;
-}
-else
-{
-current = *h;
-for (i = 0; i < idx && current != NULL; i++)
-current = current->next;
+ current = *h;
+ for (i = 0; i < idx && current != NULL; i++)
+   current = current->next;
 
-if (i < idx && current == NULL)
-{
-free(new_node);
-return (NULL);
+ if (i < idx && current == NULL)
+   {
+     free(new_node);
+     return (NULL);
+   }
 
-new_node->next = current;
-new_node->prev = current->prev;
+ new_node->next = current;
+ new_node->prev = current->prev;
+ if (current->prev != NULL)
+   current->prev->next = new_node;
+ else
+   *h = new_node;
 
-if (current->prev != NULL)
-current->prev->next = new_node;
-else
-{
-*h = new_node;
-current->prev = new_node;
-}
-}
-else
-{
-current = *h;
-while (current->next != NULL)
-current = current->next;
-new_node->prev = current;
-new_node->next = NULL;
-current->next = new_node;
-}
-
-}
+ current->prev = new_node;
+ 
 return (new_node);
 }
