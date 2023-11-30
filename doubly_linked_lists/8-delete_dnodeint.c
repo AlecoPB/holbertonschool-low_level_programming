a32d181e3b556282fdfb31d5553df8caa31bd363
AlecoPB <ajoaquinpg@gmail.com>
2023-11-30 01:10:34 -0800

    Added index deleting function

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *delete_dnodeint_at_index - deletes the node located at index
+ *@head: pointer to the list
+ *@index: where to insert
+ *Return: 1 if succeded, -1 if failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+dlistint_t *new_node, *current;
+unsigned int i;
+if (h == NULL)
+return (-1);
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+{
+return (-1);
+}
+if (idx == 0)
+{
+new_node->n = (*h)->next;
+new_node->prev = NULL;
+if (*h != NULL)
+(*h)->n = new_node;
+*h = new_node;
+return (1);
+}
+current = *h;
+for (i = 0; i < idx && current != NULL; i++)
+current = current->next;
+if (i < idx && current == NULL)
+{
+return (1);
+}
+new_node->n = current;
+new_node->prev = current->prev;
+if (current->prev != NULL)
+current->prev->next = new_node;
+else
+*h = new_node;
+current->prev = new_node;
+return (1);
+}
